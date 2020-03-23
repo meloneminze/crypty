82faecdfd08080c2c8bf6528f67de34f79816119
meloneminze <melanie@derrabus.de>
2020-03-23 17:39:55 +0100

    Moved functions

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const [command, key, value] = process.argv.slice(2);
-const { readPasswords, writePasswords } = require("./lib/passwords");
+const [command] = process.argv.slice(2);
+const { get, set, unset } = require("./lib/commands");
 
-function get() {
-  console.log("Called GET", key);
-  try {
-    const passwords = readPasswords();
-    console.log(key, passwords[key]);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// function get() {
+//   console.log("Called GET", key);
+//   try {
+//     const passwords = readPasswords();
+//     console.log(key, passwords[key]);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-function set() {
-  console.log("Called SET", key, value);
-  try {
-    const passwords = readPasswords();
-    passwords[key] = value;
-    writePasswords(passwords);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// function set() {
+//   console.log("Called SET", key, value);
+//   try {
+//     const passwords = readPasswords();
+//     passwords[key] = value;
+//     writePasswords(passwords);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-function unset() {
-  console.log("Called UNSET", key, value);
-  try {
-    const passwords = readPasswords();
-    delete passwords[key];
-    writePasswords(passwords);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// function unset() {
+//   console.log("Called UNSET", key, value);
+//   try {
+//     const passwords = readPasswords();
+//     delete passwords[key];
+//     writePasswords(passwords);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 if (command === "get") {
   get();
