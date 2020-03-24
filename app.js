53e322763bb97c7a820f83fa5844786cb7b9b714
meloneminze <melanie@derrabus.de>
2020-03-24 16:28:19 +0100

    Ask masterpassword

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const [command, key] = process.argv.slice(2);
 const { get, set, unset } = require("./lib/commands");
-const { askPassword } = require("./lib/questions");
+const { askForPassword, askForMasterPassword } = require("./lib/questions");
+const { readMasterPassword } = require("./lib/passwords");
 
 async function run() {
+  const answeredMasterPassword = await askForMasterPassword();
+  const masterPassword = readMasterPassword();
+
+  if (answeredMasterPassword !== masterPassword) {
+    console.error("Falsches Passwort");
+    return;
+  }
   if (command === "get") {
     get(key);
   } else if (command === "set") {
-    const password = await askPassword();
+    const password = await askForPassword();
     set(key, password);
   } else if (command === "unset") {
     unset(key);
